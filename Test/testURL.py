a703cf03dbab764b4b90a0ee215f5f7fd328da34
Eason <296786475@qq.com>
2024-01-05 16:57:41 +0800

    test 51job

---
+++ Test/testURL.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# https://www.zhipin.com/web/geek/job?query=ios&city=101010100&page=2
+# https://www.zhipin.com/job_detail/40fb11ee568ebdad1nNz3dW_GVNZ.html?lid=32tLhybRbXI.search.35&securityId=srxVPV2pMePF2-a1E5UWpaIAnBsblX_9dVHIoG9M-r4vSj4cLGWNVpGu4UYW7FqerBKVhtsXBZGb-FASkNoOwpspXT62s76jTrZq4eR-0bl7j5Pe&sessionId=
+def main():
+    print('Hello, World!')
+
+if __name__ == '__main__':
+    main()
+    
+    
