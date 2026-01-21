d64db0195cbf511a881ef66a261f4b22fa420dfc
Eason <hongyang.eason@gmail.com>
2026-01-21 15:03:43 +0800

    Initialize README for Python for Data Analysis study notes

---
+++ DataAnalysis/Data Wrangling/template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# filepath: /Users/adam/Documents/Developer/MyGithub/Python_Study/Proficient/recursion.py
+# coding: utf-8
+speter = "-" * 10
+
+# 第四篇
+# massage......
+
+def test():
+    pass
+if __name__ == "__main__":
+    print(f"{speter*2}Starting{speter*2}")
+    try:
+        test()
+    except KeyboardInterrupt:
+        print(f"{speter*2}手动退出程序{speter*2}")
+    finally:
+
+        print(f"{speter*2}Finished{speter*2}")
+
+
